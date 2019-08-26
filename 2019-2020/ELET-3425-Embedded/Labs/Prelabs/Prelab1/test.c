d72f790f8d66c8f1341ad049bb15b2403caae485
xylafur <james@jkesley.com>
2019-08-25 23:43:25 -0500

    Adding notes so I can access at school

---
+++ 2019-2020/ELET-3425-Embedded/Labs/Prelabs/Prelab1/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//int main ()
+//{
+//    double ch;
+//    scanf("%c", &ch);
+//    printf("%f\n", ch);
+//
+//    if(ch == 1){
+//        printf("ch is 1\n");
+//    }
+//}
+//
+int main()
+{
+    printf("%d\n", 011);
+
+}
