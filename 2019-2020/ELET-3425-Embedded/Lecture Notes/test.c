f357a1d30196b6ff10767589896812cd8ba18dcc
xylafur <jkesleyrichardson@gmail.com>
2019-09-07 15:28:46 -0500

    adding notes for my desktop

---
+++ 2019-2020/ELET-3425-Embedded/Lecture Notes/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char a = 'a';
+    switch(a){
+        case 'a':
+            printf("Got the a\n");
+            break;
+        default:
+            printf("Got the default\n");
+            break;
+    }
+}
